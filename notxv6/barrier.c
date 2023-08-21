424dc4625b37e230a251bfca3c4bec41289f6469
gyy <g18355735449@163.com>
2023-08-21 17:28:23 +0800

    123

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -28,7 +28,8 @@ static void barrier()
   pthread_mutex_lock(&bstate.barrier_mutex);
   if(++bstate.nthread < nthread) {
     pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
-  } else {
+  } 
+  else {
     bstate.nthread = 0;
     bstate.round++;
     pthread_cond_broadcast(&bstate.barrier_cond);
